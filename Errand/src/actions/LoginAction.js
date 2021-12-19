1ef8f20cec836f0542486c3495fa909116082840
go-nagyeong <ngkim0929@naver.com>
2021-12-19 16:29:08 +0900

    feat: :sparkles: Add email authentication

--- Errand/src/actions/LoginAction.js
+++ Errand/src/actions/LoginAction.js
@@ -6,7 +6,6 @@ import LoginScreen from '../screens/LoginScreen';
 export default LoginAction = (props) => {
     const [err, setErr] = useState('');
 
-    
     const signIn = (email, password) => {
         var emailReg = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
         
@@ -23,12 +22,20 @@ export default LoginAction = (props) => {
             auth()
             .signInWithEmailAndPassword(email, password)
             .then(() => {
-                setErr('');
-                props.navigation.navigate('Home');
+                var user = auth().currentUser;
+                if(user) {
+                    if(user.emailVerified) {
+                        setErr('');
+                        props.navigation.navigate('Home');
+                    } else {
+                        auth().signOut();
+                        setErr('존재하지 않는 계정입니다.');
+                    }
+                }
             })
             .catch(error => {
                 if (error.code === 'auth/user-not-found') {
-                    setErr('존재하지 않는 아이디입니다.');
+                    setErr('존재하지 않는 계정입니다.');
                 }
                 if (error.code === 'auth/wrong-password') {
                     setErr('잘못된 비밀번호입니다.');
--- Errand/src/actions/RegisterAction.js
+++ Errand/src/actions/RegisterAction.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 import RegisterScreen from '../screens/RegisterScreen';
 
@@ -11,50 +13,71 @@ export default RegisterAction = (props) => {
     const [pwErr, setPwErr] = useState("")
     const [rePwErr, setRePwErr] = useState("")
 
+    const [registrable, setRegistrable] = useState(false)
+
     const createUser = (nickname, email, password, confirmPassword, phoneNum) => {
         var emailReg = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
         var pwReg = /^.*(?=^.{6,16}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
-
-        setSubmit(true)
+                
+        setSubmit(true);
         
         if(!nickname) {
             setNameErr('이름을 입력해주세요.');
+            setRegistrable(false);
         } else {
             setNameErr('');
+            setRegistrable(true);
         }
 
         if(!email) {
             setEmailErr('이메일를 입력해주세요.');
+            setRegistrable(false);
         } else if (!emailReg.test(email)) {
             setEmailErr('이메일 형식을 올바르게 입력해주세요.');
+            setRegistrable(false);
         } else {
             setEmailErr('');
+            setRegistrable(true);
         }
 
         if(!password) {
             setPwErr('비밀번호를 입력해주세요.');
+            setRegistrable(false);
         } else if (!pwReg.test(password)) {
             setPwErr('영문, 숫자, 특수문자를 모두 포함 (6~16자 이내)');
+            setRegistrable(false);
         } else {
             setPwErr('');
+            setRegistrable(true);
         }
 
         if(!confirmPassword) {
             setRePwErr('비밀번호 재확인을 입력해주세요.');
+            setRegistrable(false);
         } else if(confirmPassword !== password) {
             setRePwErr('비밀번호가 일치하지 않습니다.');
+            setRegistrable(false);
         } else {
             setRePwErr('');
+            setRegistrable(true);
         }
 
 
-        if(email && password && !nameErr && !emailErr && !pwErr && !rePwErr) {
+        if(registrable) {
             auth()
             .createUserWithEmailAndPassword(email, password)
             .then((userCredential) => {
                 userCredential.user?.sendEmailVerification();
                 auth().signOut();
-                console.log("Email sent");
+                Alert.alert(
+                    "이메일 인증",
+                    "인증 메일을 전송하였습니다.\n전송된 이메일의 링크를 클릭하면 회원가입이 완료됩니다.",
+                    [{
+                        text: "확인",
+                        onPress: () => props.navigation.navigate('Home'),
+                        style: "cancel",
+                    }],
+                );
             })
             .catch(error => {
                 if (error.code === 'auth/email-already-in-use') {
