1a770f788122279b6afde878864aaed9c1890bed
go-nagyeong <ngkim0929@naver.com>
2022-01-07 20:21:18 +0900

    style: :lipstick: Update board UI

--- Errand/src/screens/FeedScreen.js
+++ Errand/src/screens/FeedScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {StyleSheet, Platform, SafeAreaView, useColorScheme, StatusBar, View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native';
+import {StyleSheet, Platform, SafeAreaView, useColorScheme, StatusBar, ScrollView, View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
+import FIcon from 'react-native-vector-icons/FontAwesome';
 import firestore from '@react-native-firebase/firestore';
 import Moment from 'moment';
 import 'moment/locale/ko';
@@ -8,36 +9,96 @@ import 'moment/locale/ko';
 import Container from '../components/Container';
 
 const renderItem = ({ item }) => {
+    let categoryBackgroundColor = '',
+        categoryIcon = '',
+        grade = '',
+        gradeColor = '';
+
+    switch (item.category) {
+        case '마트':
+            categoryBackgroundColor = 'green';
+            categoryIcon = 'shoppingcart'; break;
+        case '과제':
+            categoryBackgroundColor = 'lightgreen';
+            categoryIcon = 'book'; break;
+        case '탐색':
+            categoryBackgroundColor = 'brown';
+            categoryIcon = 'eyeo'; break;
+        case '서류':
+            categoryBackgroundColor = 'lightblue';
+            categoryIcon = 'filetext1'; break;
+        case '공구':
+            categoryBackgroundColor = 'gray';
+            categoryIcon = 'tool'; break;
+        case '짐':
+            categoryBackgroundColor = 'navy';
+            categoryIcon = 'car'; break;
+        case '생각':
+            categoryBackgroundColor = 'orange';
+            categoryIcon = 'bulb1'; break;
+        case '기타':
+            categoryBackgroundColor = 'lightgray';
+            categoryIcon = 'ellipsis1'; break;
+    }
+
+    if (item.writergrade >= 4.1) {
+        grade = 'A+';
+        gradeColor = '#4CA374';
+    } else if (item.writergrade >= 3.6) {
+        grade = 'A0';
+        gradeColor = '#4CA374';
+    } else if (item.writergrade >= 3.1) {
+        grade = 'B+';
+        gradeColor = '#4CA374';
+    } else if (item.writergrade >= 2.6) {
+        grade = 'B0';
+        gradeColor = '#4CA374';
+    } else if (item.writergrade >= 2.1) {
+        grade = 'C+';
+        gradeColor = '#4CA374';
+    } else if (item.writergrade >= 1.6) {
+        grade = 'C0';
+        gradeColor = '#4CA374';
+    } else if (item.writergrade >= 1.1) {
+        grade = 'D+';
+        gradeColor = '#EB4E3D';
+    } else if (item.writergrade >= 0.6) {
+        grade = 'D0';
+        gradeColor = '#EB4E3D';
+    } else {
+        grade = 'F';
+        gradeColor = '#EB4E3D';
+    }
+
     return (
         <View style={{flexDirection: 'row', backgroundColor: '#fff', height: 80, marginBottom: 15, padding: 15, borderRadius: 10}}>
             {/* 카테고리 아이콘 */}
-            <View style={{borderRadius: 30, padding: 10, marginRight: 15,
-                backgroundColor: 
-                    item.category === '대신구매' && 'orange' 
-                    || item.category === '과제' && 'green'
-                    || item.category === '기타' && 'gray'
-            }}>
-                <Icon size={30} color='white' 
-                    name={
-                        item.category === '대신구매' && 'shoppingcart' 
-                        || item.category === '과제' && 'book'
-                        || item.category === '기타' && 'plus'
-                    }
-                />
+            <View style={{backgroundColor: categoryBackgroundColor, borderRadius: 30, padding: 10, marginRight: 15}}>
+                <Icon name={categoryIcon} size={30} color='white' />
             </View>
 
             {/* 제목, 내용 */}
-            <View style={{flex: 3.8, flexDirection: 'column', flexShrink: 1, marginRight: 15}}>
-                <Text style={{fontSize: 15, fontWeight: '600', color: '#090909', marginBottom: 7}}>[{item.category}] {item.title}</Text>
-                <Text style={{fontSize: 14, color: '#89888c'}} numberOfLines={1} ellipsizeMode="tail">{item.content}</Text>
+            <View style={{flex: 3.8, flexDirection: 'column', marginRight: 15}}>
+                <Text style={{fontSize: 15, fontWeight: '600', color: '#090909', marginBottom: 7}}>
+                    {item.title}
+                </Text>
+                <Text style={{fontSize: 14, color: '#89888c'}} numberOfLines={1} ellipsizeMode="tail">
+                    {item.content}
+                </Text>
             </View>
 
-            {/* 작성일 */}
-            <View style={{flex: 1, alignSelf: 'flex-end'}}>
-                <Text style={{fontSize: 13, color: '#C2C2C2'}}>
-                {Moment(item.date.toDate()).diff(Moment(), 'days') >= -2
-                    ? Moment(item.date.toDate()).fromNow()
-                    : Moment(item.date.toDate()).format('YY/MM/DD')}
+            {/* 작성자 등급, 작성일 */}
+            <View style={{flex: 1, flexDirection: 'column'}}>
+                <View style={{flexDirection: 'row'}}>
+                    <FIcon name='user-circle' size={16} color={gradeColor} />
+                    <Text style={{top: Platform.OS=='ios'?0:-2, marginLeft: 6, fontSize: 14, fontFamily: 'Roboto-Medium', color: 'black'}}>
+                        {grade}
+                    </Text>
+                </View>
+                <Text style={{fontSize: 13, color: '#C2C2C2', marginTop: 18}}>
+                    {Moment(item.date.toDate()).diff(Moment(), 'days') >= -2
+                        ? Moment(item.date.toDate()).fromNow()
+                        : Moment(item.date.toDate()).format('YY/MM/DD')}
                 </Text>
             </View>
         </View>
@@ -49,10 +110,6 @@ export default HomeScreen = (props) => {
 
     const [keyword, setKeyword] = useState('');
 
-    useEffect(() => {
-        props.getFeed();
-    }, [])
-
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -61,15 +118,11 @@ export default HomeScreen = (props) => {
             </View>
 
             <View style={styles.boardView}>
-                <View style={styles.boardHeader}>
-                    <View style={styles.search} >
-                        <TextInput style={styles.searchBox} placeholder="search" value={keyword} onChangeText={text => setKeyword(text)}/>
-                        <TouchableOpacity style={styles.searchButton}>
-                            <Icon name="search1" size={20} color="white" />
-                        </TouchableOpacity>
-                    </View>
-                    
-                    <Text style={styles.filter}>필터 위치</Text>
+                <View style={styles.search} >
+                    <TextInput style={styles.searchBox} placeholder="search" value={keyword} onChangeText={text => setKeyword(text)}/>
+                    <TouchableOpacity style={styles.searchButton} onPress={() => props.search(keyword)}>
+                        <Icon name="search1" size={20} color="white" />
+                    </TouchableOpacity>
                 </View>
                 
                 <FlatList 
@@ -91,22 +144,32 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#53B77C',
     },
+    categoryBox: {
+        backgroundColor: '#fff',
+        padding: 18,
+        borderRadius: 30,
+        width: 100,
+        height: 100,
+        marginRight: 15,
+    },
+    categoryText: {
+        color: 'black',
+        fontFamily: 'Roboto-Medium',
+        fontSize: 16,
+    },
     boardView: {
-        flex: 3,
+        flex: Platform.OS === 'ios' ? 2.6 : 2,
         backgroundColor: '#EDF1F5',
         padding: 12,
         paddingVertical: 40,
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
     },
-    boardHeader: {
-        alignItems: 'center',
-    },
     search: {
         position: 'absolute',
-        top: -65, 
-        left: 5,
-        right: 5, 
+        top: -25, 
+        left: 20,
+        right: 20, 
     },
     searchBox: {
         backgroundColor: '#fff',
@@ -122,9 +185,5 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center',
         borderRadius: 30,
-    },
-    filter: {
-        fontSize: 20,
-        marginBottom: 10,
     },
 });
